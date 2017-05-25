fa085c86ece02a794ba6a622ae17560ace7f9327
StarryPurp <starbloom128@gmail.com>
2017-05-25 19:29:09 +0900

    #4 Menu Activity all

--- app/src/main/java/bstar128/example/hansung/contextmenutest/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/contextmenutest/MainActivity.java
@@ -1,20 +1,31 @@
 package bstar128.example.hansung.contextmenutest;
 
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but2;
+    ImageView pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1=(Button)findViewById(R.id.but1);
-        Button but2=(Button)findViewById(R.id.but2);
+        but2=(Button)findViewById(R.id.but2);
+        linear=(LinearLayout)findViewById(R.id.linear) ;
+        pic=(ImageView)findViewById(R.id.pic1) ;
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -33,4 +44,30 @@ public class MainActivity extends AppCompatActivity {
             menuInflater.inflate(R.menu.menu2,menu);
         }
     }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink:
+                linear.setBackgroundColor(Color.rgb(255,178,217));
+                return true;//rgb색상표로 가능!
+            case R.id.menu1_item_yellow:
+                linear.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.menu2_pic_rotation:
+                pic.setRotation(pic.getRotation()+90);
+                return true;
+            case R.id.menu2_pic_size:
+                pic.setScaleX(1.5f);
+                pic.setScaleY(1.5f);//1.5배씩 배수로 커짐
+                return true;
+        }
+
+        return false;
+    }
 }
+
